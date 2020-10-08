da96da6b0af72332694f60b953b0f795df68dc25
abyzziboll <juan_carlos.js@hotmail.com>
2020-10-08 17:17:56 -0300

    final do curso

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('',views.home,name="home"),
-    path('examples',views.examples,name="examples")
+    path('examples',views.examples,name="examples"),
+    path('post/<int:pk>/',views.detalhe_post, name = "detalhe_post"),
+    path('post/new/',views.adicionar_postagem, name = "adicionar_postagem"),
+    path('post/edit/<int:pk>/',views.edit_postagem, name = "edicao_postagem")
 ]
